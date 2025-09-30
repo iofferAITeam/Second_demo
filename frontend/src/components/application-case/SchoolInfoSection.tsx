a38c228766edfef2bed8c5eac276c36cf3a967f0
jay17an <zijiehan0406@gmail.com>
2025-09-30 03:20:34 -0700

    update SchoolInforSection.tsx

--- frontend/src/components/application-case/SchoolInfoSection.tsx
+++ frontend/src/components/application-case/SchoolInfoSection.tsx
@@ -15,26 +15,74 @@ export default function SchoolInfoSection() {
         <h2 className="user-name">Nickyouth</h2>
       </div>
 
-      {/* 学校列表 */}
-      <div className="school-list">
-        <p>California College of the Arts – MA Interaction Text TextTextText Design</p>
-        <p>California College of the Arts – MA Interaction Text TextTextTextText Design</p>
-        <p>University name – Major name</p>
+      {/* Offer Results */}
+      <div className="offer-results">
+        <ul>
+          <li>
+            <span className="offer-label">Offer results</span>
+            <div className="offer-item">
+              <Image
+                src="/images/icon-24-通用.svg"
+                alt="school icon"
+                width={16}
+                height={16}
+                className="offer-icon"
+              />
+              <span>California College of the Arts – MA Interaction Text TextTextText Design</span>
+            </div>
+          </li>
+          <li>
+            <span className="offer-label"></span>
+            <div className="offer-item">
+              <Image
+                src="/images/icon-24-通用.svg"
+                alt="school icon"
+                width={16}
+                height={16}
+                className="offer-icon"
+              />
+              <span>California College of the Arts – MA Interaction Text TextTextTextText Design TextTextTextText</span>
+            </div>
+          </li>
+          <li>
+            <span className="offer-label"></span>
+            <div className="offer-item">
+              <Image
+                src="/images/icon-24-通用.svg"
+                alt="school icon"
+                width={16}
+                height={16}
+                className="offer-icon"
+              />
+              <span>University name – Major name</span>
+            </div>
+          </li>
+        </ul>
       </div>
 
       {/* 背景信息 */}
-      <div className="background-tags">
-        <span className="tag">GPA 3.50</span>
-        <span className="tag">IELTS 6.5</span>
-        <span className="tag">Intended Major: Interaction Design</span>
-        <span className="tag">Target Country: US</span>
-      </div>
+      <div className="background">
+        <div className="background-row">
+          <span className="offer-label">Background</span>
+          <div className="background-tags">
+            <span className="tag">GPA 3.80</span>
+            <span className="tag">IELTS 6.5</span>
+            <span className="tag">Intended Major: Interaction Design</span>
+            <span className="tag">Target Country: UK</span>
+          </div>
+        </div>
 
-      {/* 个人标签 */}
-      <div className="profile-tags">
-        <span className="profile-tag">Unique Personal Profile tag</span>
-        <span className="profile-tag">Unique Personal Profile tag</span>
-        <span className="profile-tag">Unique Personal Profile tag</span>
+        <div className="background-row">
+          <span className="section-label"></span>
+          <div className="profile-tags">
+            <span className="profile-tag">Unique Personal Profile tag</span>
+            <span className="profile-tag">Unique Personal Profile tag</span>
+            <span className="profile-tag">Unique Personal Profile tag</span>
+            <span className="profile-tag">Unique Personal Profile tag</span>
+            <span className="profile-tag">Unique Personal Profile tag</span>
+            <span className="profile-tag">Unique Personal Profile tag</span>
+          </div>
+        </div>
       </div>
     </section>
   );
